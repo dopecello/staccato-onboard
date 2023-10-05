e311e115e97dcd79546b45db479d54ec796c0940
William Locke <williamlocke.dev@gmail.com>
2023-10-05 11:43:50 -0400

    initial spring boot commit

---
+++ src/main/java/com/staccato/onboard/OnboardApplication.java
@@ -0,0 +1,14 @@
+package com.staccato.onboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
