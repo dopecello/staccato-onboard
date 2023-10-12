698bb823ca6d9f55a788c49c731596455bbd79ef
William Locke <williamlocke.dev@gmail.com>
2023-10-11 21:37:07 -0400

    added annotations to Musician model

--- src/main/java/com/staccato/onboard/model/Musician.java
+++ src/main/java/com/staccato/onboard/model/Musician.java
@@ -1,8 +1,14 @@
 package com.staccato.onboard.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.Date;
 
+@Document("musicianModel")
 public class Musician {
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
